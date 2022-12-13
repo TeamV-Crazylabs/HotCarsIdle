0587ede8f91740a294d591dbbb9bcc0258122f7e
Ardil <ardilkgokce.edu@gmail.com>
2022-12-13 19:43:07 +0300

    Flag Mechanic Added

---
+++ TeamV_HotCarsIdle_12122022_Crazylabs/Assets/Scripts/Flag.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            //1 dolar partcile play
+            Money.totalDollars += 1;
+            Debug.Log(Money.totalDollars);
+        }
+        else if (collision.gameObject.layer == 11)
+        {
+            //1 dolar partcile play
+            Money.totalDollars += 5;
+        }
+        else if (collision.gameObject.layer == 12)
+        {
+            //1 dolar partcile play
+            Money.totalDollars += 25;
+        }
+    }
+}
---
+++ TeamV_HotCarsIdle_12122022_Crazylabs/Assets/Scripts/Money.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public static int totalDollars;
+    void Start()
+    {
+        //playerprefs
+        totalDollars= 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

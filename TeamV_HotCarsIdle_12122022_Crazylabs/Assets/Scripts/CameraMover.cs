ae9213abd26b44f5c795b74e02c731243c1ffabd
Ardil <ardilkgokce.edu@gmail.com>
2022-12-15 02:35:57 +0300

    CameraRotation Added

---
+++ TeamV_HotCarsIdle_12122022_Crazylabs/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float rotationSpeed = 0.5f;
+
+    public static bool DebugMode = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount == 1)
+        {
+            Touch screenTouch = Input.GetTouch(0);
+
+            if (screenTouch.phase == TouchPhase.Moved)
+            {
+                transform.Rotate(0f, screenTouch.deltaPosition.x * rotationSpeed, 0f);
+                
+            }
+
+        }
+
+        if (DebugMode)
+        {
+            if (Input.touchCount == 1)
+            {
+                Touch screenTouch = Input.GetTouch(0);
+
+                if (screenTouch.phase == TouchPhase.Moved)
+                {
+                    transform.Rotate(0f, screenTouch.deltaPosition.x * rotationSpeed, 0f);
+
+                    transform.Rotate(screenTouch.deltaPosition.y * rotationSpeed * -1, 0f, 0f);
+                }
+
+            }
+        }
+    }
+}

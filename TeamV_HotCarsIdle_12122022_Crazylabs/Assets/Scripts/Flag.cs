f5a20d978f4e36c51e49eb609f9b71cd1ef5b605
Ardil <ardilkgokce.edu@gmail.com>
2022-12-15 00:01:52 +0300

    Dolar Particle Added

--- TeamV_HotCarsIdle_12122022_Crazylabs/Assets/Scripts/Flag.cs
+++ TeamV_HotCarsIdle_12122022_Crazylabs/Assets/Scripts/Flag.cs
@@ -4,37 +4,45 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
+    [SerializeField] ParticleSystem Dollar1Particle;
+    [SerializeField] ParticleSystem Dollar5Particle;
+    [SerializeField] ParticleSystem Dollar25Particle;
+    [SerializeField] ParticleSystem Dollar125Particle;
+    [SerializeField] ParticleSystem Dollar625Particle;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 10)
         {
             //1 dolar partcile play
             Money.totalDollars += 1;
+            Dollar1Particle.Play();
         }
         else if (collision.gameObject.layer == 11)
         {
             //1 dolar partcile play
             Money.totalDollars += 5;
+            Dollar5Particle.Play();
         }
         else if (collision.gameObject.layer == 12)
         {
             //1 dolar partcile play
             Money.totalDollars += 25;
+            Dollar25Particle.Play();
         }
         else if (collision.gameObject.layer == 13)
         {
             //1 dolar partcile play
             Money.totalDollars += 125;
+            Dollar125Particle.Play();
         }
         else if (collision.gameObject.layer == 14)
         {
             //1 dolar partcile play
             Money.totalDollars += 625;
+            Dollar625Particle.Play();
         }
-        else if (collision.gameObject.layer == 15)
-        {
-            //1 dolar partcile play
-            Money.totalDollars += 25;
-        }
+        
     }
+    
+    
 }
